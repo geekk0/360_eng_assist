264e39372eb3087982307fd430c1ce0e1f06a0b5
geekk0 <gekk0dw@gmail.com>
2021-01-20 18:00:21 +0300

    ntoken

--- 360_eng_assist.py
+++ 360_eng_assist.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 import time
-from token import TOKEN
+from ntoken import TOKEN
 from config import dest_cameras, dest_schemes, dest_ZOOM, dest_script
 from DB import Files, Schemes, ZOOM
 
