934c702fc284dc3af1a221e6c232db0674d277ba
geekk0 <gekk0dw@gmail.com>
2021-01-22 20:44:37 +0300

    russian buttons

--- 360_eng_assist.py
+++ 360_eng_assist.py
@@ -2,18 +2,19 @@ import telebot
 import os
 import time
 from ntoken import TOKEN
-from config import dest_cameras, dest_schemes, dest_ZOOM, dest_script
+from config import dest_cameras, dest_schemes, dest_ZOOM
 from DB import Files, Schemes, ZOOM
 from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
-commands_for_buttons = ['otpuska', 'ip_adr', 'asb3bank']
+commands_for_buttons = {'otpuska': 'Отпуска', 'ip_adr': 'ip', 'asb3bank': 'asb3bank', 'schemes_list': 'Схемы', 'cameras_list': 'Камеры', 'zoom_list': 'Zoom'}
 
 
 def create_keyboard():
-    keyboard = types.InlineKeyboardMarkup(row_width=3)
-    buttons = [types.InlineKeyboardButton(text=c, callback_data=c) for c in commands_for_buttons]
+    keyboard = types.InlineKeyboardMarkup(row_width=4)
+    buttons = [types.InlineKeyboardButton(text=commands_for_buttons[key], callback_data=key)
+    for key in commands_for_buttons.keys()]
     keyboard.add(*buttons)
     return keyboard
 
@@ -37,31 +38,30 @@ def ip_adr(messageid):
     keyboard = create_keyboard()
     bot.send_document(messageid, ip_adr, reply_markup=keyboard)
 
+
 @bot.message_handler(commands=['cameras'])
-def list(message):
+def cameras_list(messageid):
     for key in Files:
-        bot.send_message(message.chat.id, key)
+        bot.send_message(messageid, key)
 
 
 @bot.message_handler(commands=['schemes'])
-def list(message):
+def schemes_list(messageid):
     for key in Schemes:
-        bot.send_message(message.chat.id, key)
+        bot.send_message(messageid, key)
 
 
 @bot.message_handler(commands=['ZOOM'])
-def list(message):
+def zoom_list(messageid):
     for key in ZOOM:
-        bot.send_message(message.chat.id, key)
+        bot.send_message(messageid, key)
+
 
 @bot.message_handler(commands=['asb3bank'])
 def asb3bank(messageid):
     asb3bank = open('asb3bank.png', 'rb')
     keyboard = create_keyboard()
     bot.send_document(messageid, asb3bank, reply_markup=keyboard)
-
-def testfunc(messageid):
-    bot.send_message(messageid, 'testfuncworks')
 
 
 @bot.callback_query_handler(func=lambda x: True)
@@ -74,27 +74,22 @@ def callback_handler(callback_query):
 
 @bot.message_handler(content_types=["text"])
 def arrangement(message):
-    coin_num = 0
+
     for key in Files:
         if message.text.lower() == key:
-            coin_num = 1
             img = open(dest_cameras + Files.get(key), 'rb')
             bot.send_photo(message.chat.id, img)
             break
     for key in Schemes:
         if message.text.lower() == key:
-            coin_num = 2
             doc = open(dest_schemes + Schemes.get(key), 'rb')
             bot.send_document(message.chat.id, doc)
             break
     for key in ZOOM:
         if message.text.lower() == key:
-            coin_num = 3
             img = open(dest_ZOOM + ZOOM.get(key), 'rb')
             bot.send_photo(message.chat.id, img)
             break
-    if coin_num == 0:
-        bot.send_message(message.chat.id, 'нет такой команды')
 
 
 if __name__ == '__main__':
